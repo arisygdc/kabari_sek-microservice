cde9da8caf4efce142fe7bceb6dfa1abf515ba36
arisygdc <arisycso@gmail.com>
2022-05-16 09:55:39 +0700

    remove friend table

--- micro-user/db/migration/000001_init_schema.up.sql
+++ micro-user/db/migration/000001_init_schema.up.sql
@@ -10,25 +10,14 @@ CREATE TABLE "auth_user" (
   PRIMARY KEY ("auth_id", "user_id")
 );
 
-CREATE TABLE "user" (
+CREATE TABLE "users" (
   "id" uuid PRIMARY KEY,
   "firstname" VARCHAR NOT NULL,
   "lastname" VARCHAR NOT NULL,
   "birth" date NOT NULL,
-  "created_at" TIMESTAMP NOT NULL
-);
-
-CREATE TABLE "friends" (
-  "user1" uuid,
-  "user2" uuid,
-  "approved_at" timestamp,
-  PRIMARY KEY ("user1", "user2")
+  "created_at" TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
 ALTER TABLE "auth_user" ADD FOREIGN KEY ("auth_id") REFERENCES "auth" ("id");
 
 ALTER TABLE "auth_user" ADD FOREIGN KEY ("user_id") REFERENCES "user" ("id");
-
-ALTER TABLE "user" ADD FOREIGN KEY ("id") REFERENCES "friends" ("user1");
-
-ALTER TABLE "user" ADD FOREIGN KEY ("id") REFERENCES "friends" ("user2");
