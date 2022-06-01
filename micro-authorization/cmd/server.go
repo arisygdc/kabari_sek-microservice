0f8025a271c3ed94121b10eeca23b0508370574b
arisygdc <arisycso@gmail.com>
2022-06-02 02:14:47 +0700

    handler

--- micro-authorization/cmd/server.go
+++ micro-authorization/cmd/server.go
@@ -1,5 +1,41 @@
 package main
 
-func main() {
+import (
+	"chat-in-app_microservices/micro-authorization/config"
+	"chat-in-app_microservices/micro-authorization/core/api"
+	"chat-in-app_microservices/micro-authorization/core/authorization"
+	"chat-in-app_microservices/micro-authorization/db"
+	"chat-in-app_microservices/micro-authorization/pb"
+	"context"
+	"log"
 
+	"go-micro.dev/v4"
+)
+
+func main() {
+	cfg := config.Config{}
+	err := config.LoadConfig(".", "config", &cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	svcServer := micro.NewService(
+		micro.Name(cfg.Service.Name),
+	)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	repos, err := db.NewRepository(ctx, cfg.Database)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	svcAuthorization := authorization.NewAuthorization(repos)
+	grpcHandler := api.NewGrpcHandler(svcAuthorization)
+
+	err = pb.RegisterAuthorizationHandler(svcServer.Server(), grpcHandler)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
