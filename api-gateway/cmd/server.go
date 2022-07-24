80471c52f1418b48691041050614d13429641424
arisygdc <arisycso@gmail.com>
2022-07-24 16:41:04 +0700

    remove http mux router

--- api-gateway/cmd/server.go
+++ api-gateway/cmd/server.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"chat-in-app_microservices/api-gateway/api"
 	"chat-in-app_microservices/api-gateway/clientapi"
 	"chat-in-app_microservices/api-gateway/config"
 	"log"
-
-	"github.com/gorilla/mux"
 )
 
 func main() {
@@ -16,15 +13,8 @@ func main() {
 		log.Fatal(err)
 	}
 
-	svcClient := clientapi.NewService(cfg.Endpoint)
+	clientapi.NewService(cfg.Endpoint)
 
-	engine := mux.NewRouter()
-	handler := api.NewHandler(cfg.Server, engine)
-
-	router := api.NewRouter(cfg.Server, svcClient)
-	router.RegisterRoute(handler)
-
-	err = handler.Serve()
 	if err != nil {
 		log.Fatal(err)
 	}
