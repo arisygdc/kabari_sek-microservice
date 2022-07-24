d02d196e8676dda8b8b760b222dcb4f2bf8a396f
arisygdc <arisycso@gmail.com>
2022-07-24 09:16:00 +0700

    update proto

--- micro-user/core/user/router.go
+++ micro-user/core/user/router.go
@@ -23,18 +23,10 @@ func (r UserRouter) Login(ctx context.Context, req *pb.LoginRequest, res *pb.Log
 	loginParam := Auth{username: req.Username, password: req.Password}
 	token, err := r.svc.Login(ctx, loginParam, r.conf.Token.Secret, r.conf.Token.Dur)
 	if err != nil {
-		if err == ErrIncorrectUserOrPass {
-			res.ResponseCode = http.StatusForbidden
-			res.Message = err.Error()
-		}
-
-		res.ResponseCode = http.StatusInternalServerError
-		res.Message = http.StatusText(int(res.ResponseCode))
 		return err
 	}
 
 	res.ResponseCode = http.StatusAccepted
-	res.Message = http.StatusText(int(res.ResponseCode))
 	res.UserToken = token
 	return nil
 }
@@ -42,7 +34,6 @@ func (r UserRouter) Login(ctx context.Context, req *pb.LoginRequest, res *pb.Log
 func (r UserRouter) Register(ctx context.Context, req *pb.RegisterRequest, res *pb.RegisterResponse) error {
 	birth, err := ParseTime(req.Birth)
 	if err != nil {
-		res.ResponseCode = http.StatusBadRequest
 		res.Message = err.Error()
 		return err
 	}
@@ -61,12 +52,10 @@ func (r UserRouter) Register(ctx context.Context, req *pb.RegisterRequest, res *
 
 	err = r.svc.Register(ctx, registerParam)
 	if err != nil {
-		res.ResponseCode = http.StatusInternalServerError
-		res.Message = http.StatusText(int(res.ResponseCode))
+		res.Message = http.StatusText(http.StatusInternalServerError)
 		return err
 	}
 
-	res.ResponseCode = http.StatusAccepted
-	res.Message = http.StatusText(int(res.ResponseCode))
+	res.Message = http.StatusText(http.StatusAccepted)
 	return nil
 }
